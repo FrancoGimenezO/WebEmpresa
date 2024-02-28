5fb04d56c9429a6c3581af626ce4996ae2ff4bb1
FrancoGimenezO <francogz19980427@gmail.com>
2024-02-28 15:43:15 -0300

    actualizadoservices

--- services/models.py
+++ services/models.py
@@ -6,7 +6,7 @@ class Service(models.Model):
     title = models.CharField(max_length = 200, verbose_name = "Titulo")
     subtitle = models.TextField(max_length = 200, verbose_name = "Subtitulo")
     content = models.TextField(verbose_name = "Contenido")
-    image = models.ImageField(verbose_name = "Imagen", upload_to="servicesw")
+    image = models.ImageField(verbose_name = "Imagen", upload_to="services")
     created = models.TimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.TimeField(auto_now=True, verbose_name="Fecha de modificación")
 
