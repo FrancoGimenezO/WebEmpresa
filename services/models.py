2387ae68223f940804af1980c8274c0cfefe59bd
FrancoGimenezO <francogz19980427@gmail.com>
2024-02-28 15:12:39 -0300

    appservices

--- services/models.py
+++ services/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-class Project(models.Model):
+class Service(models.Model):
     title = models.CharField(max_length = 200, verbose_name = "Titulo")
     subtitle = models.TextField(max_length = 200, verbose_name = "Subtitulo")
     content = models.TextField(verbose_name = "Contenido")
-    image = models.ImageField(verbose_name = "Imagen", upload_to="media/projects")
+    image = models.ImageField(verbose_name = "Imagen", upload_to="servicesw")
     created = models.TimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.TimeField(auto_now=True, verbose_name="Fecha de modificación")
 
 
     class Meta:
-        verbose_name = "proyecto"
-        verbose_name_plural = "proyectos"
-        ordering = ["-created"]
+        verbose_name = "Servicio"
+        verbose_name_plural = "Servicios"
+        ordering = ['-created']
 
     def __str__(self):
         return self.title
